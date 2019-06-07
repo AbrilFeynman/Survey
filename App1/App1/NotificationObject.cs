377c4684080cfc031b0b4942929e294748ef8cf7
QuasarAbril <feynmanquantumthings@gmail.com>
2019-06-07 15:01:19 -0500

    Changes

---
+++ App1/App1/NotificationObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1
+{
+    //abstract por que no hay instancias solo herencias
+   public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
